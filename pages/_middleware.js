547a504d5ff15bb6aee5f03a4800d476257d7276
samsonkassa-dev <samsonkassa25@gmail.com>
2024-10-17 11:00:23 +0300

    auth removed from middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,11 +4,11 @@ export default function middleware(req) {
   const { cookies } = req;
   const jwt = cookies.jwt;
   const url = req.url;
-  if (url.includes('/application')) {
-    if (jwt === undefined) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
-    return NextResponse.next();
-  }
+  // if (url.includes('/application')) {
+  //   if (jwt === undefined) {
+  //     return NextResponse.redirect(new URL('/login', req.url));
+  //   }
+  //   return NextResponse.next();
+  // }
   return NextResponse.next();
 }
